fd5cc1cb478ceda3a6d6a659e259d46153a70bf7
jsweb4dev <jsweb4dev@gmail.com>
2024-09-19 17:08:13 -0400

    finished

---
+++ main.js
@@ -0,0 +1,14 @@
+
+const list = new LinkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+console.log(list)
+
+console.log(list.toString())
--- script.js
+++ script.js
@@ -21,16 +21,15 @@ class LinkedList {
             let node = this.head;
             //console.log(node.next)
             //node.next = null
-            if (node.next !== null) {
-                node.next
+            while(node.next !== null) {
+                node = node.next
             }
-            else if(node.next === null){
+            if(node.next === null){
                     //console.log("i got here")
-                    node.next = new Node()
+                    node.next = new Node(value)
                     node.next.value = value
-                    return node.next
+                    return node
                 }
-                
             }
 
     prepend(value){
@@ -108,13 +107,12 @@ class LinkedList {
             
         } if ( current.next === null) {
             
-            //delete previous.next KINDA works
-            console.log(previous, "previous")
-            console.log(current, "current")
+            //console.log(previous, "previous")
+            //console.log(current, "current")
 
             previous.next = null
 
-            console.log(previous)
+            //console.log(previous)
             
             return current
         }
@@ -167,41 +165,115 @@ class LinkedList {
 
     toString(){
 
-        let printDiv = document.createElement("div")
-        printDiv.setAttribute("id", "printDiv")
+        let printDiv = document.querySelector("#print")
+            //console.log(printDiv)
         
-        document.body.appendChild(printDiv)
+        let node = this.head;
+        //console.log("I am working on to string below HERE \/")
+        //console.log(node)
 
-        console.log(printDiv)
+        if(node.next === null) {return null}
 
-        return "print"
+        let html = "";
+
+        html += node.value + " -> "
+        printDiv.innerHTML += html
+
+        while(node.next !== null){
+            node = node.next
+            html = `${node.value} -> `
+            printDiv.innerHTML += html
+
+        } if (node.next === null){
+            html = " null"
+            printDiv.innerHTML += html
+        }
+
+            return "print"
     }
+
+    insertAt(value, index){
+
+        let arr = [];
+        let current = this.head;
+        arr.push(current)
+        let previous = null;
+
+        if(current.next === null) { return null }
+
+        while(current.next !== null){
+
+            previous = current;
+            current = current.next;
+            arr.push(current)
+
+            
+        if (arr[index]){
+            let currentOld = current
+            //console.log(currentOld)
+
+            current = new Node()
+            current.value = value;
+            current.next = currentOld
+
+            let node = this.head
+            previous.next = current
+            return node
+                }
+            }
+    }
+
+    removeAt(index){
+        let arr = [];
+        let current = this.head;
+        arr.push(current)
+        let previous = null;
+
+        if(current.next === null) { return null }
+
+        while(current.next !== null){
+
+            previous = current;
+            current = current.next;
+            arr.push(current)
+
+        } if (arr[index]){
+
+
+            let prevNode = arr[index - 1]
+            //console.log(prevNode, " node before the target")
+            //console.log(previous, "prev var and target to del")
+            //console.log(current, "current")
+
+            prevNode.next = null
+            prevNode.next = current
+            
+            let node = this.head
+            return node
+        }
+        else {
+            return "index not found for deletion"
+        }
+    }
+
 
 }
 
 
+/*
 let myList = new LinkedList()
 console.log(myList)
 console.log(myList.append(5))
 console.log(myList.append(10))
 console.log(myList.prepend(2))
-console.log(myList)
 console.log(myList.size())
 console.log(myList.getHead())
 console.log(myList.getTail())
-
-
-console.log(myList.atIndex(5))
-
-console.log("above is the completed indexAT")
-
+console.log(myList.atIndex(1))
 console.log(myList.pop())
-
-console.log(myList)
-
 console.log(myList.contains(5))
-
 console.log(myList.find(2))
-
 console.log(myList.toString())
-
+console.log(myList.insertAt(9, 1))
+console.log(myList.removeAt(1))
+*/
